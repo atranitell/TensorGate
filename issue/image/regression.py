71741e522e06fcf10b59656158af0d9eb1e261dd
atranitell <atranitell@gmail.com>
2017-04-16 11:12:49 +0800

    small adjust

--- issue/image/regression.py
+++ issue/image/regression.py
@@ -448,8 +448,7 @@ def test_heatmap(name, net_name, chkp_path=None, summary_writer=None):
                 str_label = 'label:%d' % int(label)
                 str_predict = 'predict:%.2f' % float(predict)
                 plt.title(str_label+'    '+str_predict)
-                plt.xlabel(pp_name+' '+img_base_name+'.jpg')
-                # plt.axis('off')
+                # plt.xlabel(pp_name+' '+img_base_name+'.jpg')
                 plt.xticks([], [])
                 plt.yticks([], [])
 
