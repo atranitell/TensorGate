20ca42a7435cfc53514227bea154cdf5f4bfdc37
atranitell <atranitell@gmail.com>
2017-05-06 17:57:34 +0800

    tidy up analysis and add readme

--- analysis/avec2014_error.py
+++ analysis/avec2014_error.py
@@ -1,24 +1,169 @@
 
 import os
-from analysis import avec2014_textfile
-from analysis import error
+import math
+import re
+import numpy as np
+
+""" Input: a log file
+    Output: {'class1':[label, logit], ...}
+"""
+
+
+def get_seq_list(path):
+    """ For video input
+        class1 3 2.53
+        class2 4 4.67
+        ...
+    Return:
+        {'class1':[label, logit], ...}
+    """
+    result = {}
+    res_fp = open(path, 'r')
+    for line in res_fp:
+        r1 = re.findall('frames_flow\\\\(.*)_video', line)
+        r2 = re.findall('frames\\\\(.*)_video', line)
+
+        res = r1[0] if len(r1) else r2[0]
+
+        label = re.findall(' (.*?) ', line)
+        logit = re.findall(label[0] + ' (.*)\n', line)
+
+        label = float(label[0])
+        logit = float(logit[0])
+
+        result[res] = [label, logit]
+    return result
+
+
+def get_succ_list(path):
+    """ For succ video sequence
+    """
+    res_fp = open(path, 'r')
+    res_label = {}
+    res_logit = {}
+    for line in res_fp:
+        r1 = re.findall('frames_flow\\\\(.*)_video', line)
+        r2 = re.findall('frames\\\\(.*)_video', line)
+
+        res = r1[0] if len(r1) else r2[0]
+
+        if res.find('frames') >= 0:
+            print(res)
+
+        label = re.findall(' (.*?) ', line)
+        logit = re.findall(label[0] + ' (.*)\n', line)
+
+        if res not in res_label:
+            res_label[res] = [float(label[0])]
+        else:
+            res_label[res].append(float(label[0]))
+
+        logit_f = float(logit[0])
+
+        if res not in res_logit:
+            res_logit[res] = [logit_f]
+        else:
+            res_logit[res].append(logit_f)
+
+    # acquire mean
+    result = {}
+    for idx in res_label:
+        label = np.mean(np.array(res_label[idx]))
+        logit = np.mean(np.array(res_logit[idx]))
+        result[idx] = [label, logit]
+
+    return result
+
+
+def get_img_list(path):
+    """ For image input
+        class1 2 3.56
+        class1 2 4.32
+        class1 2 1.23
+        ...
+        class2 3 2.11
+        ...
+    Return:
+        Note: the logit will be mean of the value,
+          because the label is same value in same class
+        {'class1':[label, logit], ...}
+    """
+    res_fp = open(path, 'r')
+    res_label = {}
+    res_logit = {}
+    for line in res_fp:
+        r1 = re.findall('frames_flow\\\(.*?)_video', line)
+        r2 = re.findall('frames\\\(.*?)_video', line)
+
+        res = r1[0] if len(r1) else r2[0]
+
+        label = re.findall(' (.*?) ', line)
+        logit = re.findall(label[0] + ' (.*)\n', line)
+
+        if res not in res_label:
+            res_label[res] = [float(label[0])]
+        else:
+            res_label[res].append(float(label[0]))
+
+        logit_f = float(logit[0])
+
+        if res not in res_logit:
+            res_logit[res] = [logit_f]
+        else:
+            res_logit[res].append(logit_f)
+
+    # acquire mean
+    result = {}
+    for idx in res_label:
+        label = np.mean(np.array(res_label[idx]))
+        logit = np.mean(np.array(res_logit[idx]))
+        result[idx] = [label, logit]
+    return result
+
+
+def get_mae_rmse(res):
+    """
+    Input: a dict
+        {'class1':[label value],
+         'class2':[label value]}
+    """
+    mae = 0.0
+    rmse = 0.0
+    for idx in res:
+        mae += abs(res[idx][0] - res[idx][1])
+        rmse += math.pow(res[idx][0] - res[idx][1], 2)
+    mae = mae / len(res)
+    rmse = math.sqrt(rmse / len(res))
+    return mae, rmse, len(res)
+
+
+def get_mae(res):
+    mae = 0.0
+    for idx in res:
+        mae += abs(res[idx][0] - res[idx][1])
+    mae = mae / len(res)
+    return mae, len(res)
+
+
+def get_rmse(res):
+    rmse = 0.0
+    for idx in res:
+        rmse += math.pow(res[idx][0] - res[idx][1], 2)
+    rmse = math.sqrt(rmse / len(res))
+    return rmse, len(res)
 
 
 def get_accurate_from_file(path, data_type):
 
     if data_type == 'seq':
-        res = error.get_mae_rmse(avec2014_textfile.get_seq_list(path))
+        res = get_mae_rmse(get_seq_list(path))
     elif data_type == 'img':
-        res = error.get_mae_rmse(avec2014_textfile.get_img_list(path))
+        res = get_mae_rmse(get_img_list(path))
     elif data_type == 'succ':
-        res = error.get_mae_rmse(avec2014_textfile.get_succ_list(path))
+        res = get_mae_rmse(get_succ_list(path))
 
     mae, rmse, count = res
     return mae, rmse
-
-
-def get_mae_rmse(res):
-    return error.get_mae_rmse(res)
 
 
 def get_all_res_in_fold(path, data_type):
@@ -32,11 +177,11 @@ def get_all_res_in_fold(path, data_type):
         filepath = os.path.join(path, filename)
 
         if data_type == 'seq':
-            res = get_mae_rmse(avec2014_textfile.get_seq_list(filepath))
+            res = get_mae_rmse(get_seq_list(filepath))
         elif data_type == 'img':
-            res = get_mae_rmse(avec2014_textfile.get_img_list(filepath))
+            res = get_mae_rmse(get_img_list(filepath))
         elif data_type == 'succ':
-            res = get_mae_rmse(avec2014_textfile.get_succ_list(filepath))
+            res = get_mae_rmse(get_succ_list(filepath))
 
         if res[0] < min_mae:
             min_mae = res[0]
--- analysis/tfevents_draw_test.py
+++ analysis/tfevents_draw_test.py
@@ -2,11 +2,8 @@
     python utils/draw_test.py
 """
 
-import sys
-sys.path.append('.')
-
-import analysis.draw as draw
-import analysis.data_tfevents as tfevents
+import tfevents_draw
+import tfevents
 
 
 # get specify data
@@ -14,10 +11,13 @@ tags, iter_tag = tfevents.get_tags('regression', 'train')
 
 # parse data
 values = tfevents.get_image_summary(
-    '../_output/MODEL/avec2014_train_inception_resnet_v2_fold0', tags, iter_tag)
+    'C:/Users/jk/Desktop/Gate/_output/avec2014_train_201705060006', tags, iter_tag)
 
 # transfer data to draw
 info = tfevents.get_info_data(values, tags, iter_tag)
 
+# ['train/lr', 'train/err_mae', 'train/err_mse', 'train/loss']
+print(tags)
+
 # draw data
-draw.draw(info, tags, iter_tag)
+tfevents_draw.draw(info, tags[3], iter_tag)
