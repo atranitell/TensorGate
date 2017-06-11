8fdff3180bdd15f4ea779e7d5f5c17385415db11
atranitell <atranitell@gmail.com>
2017-06-11 22:23:15 +0800

    celeba for gan

--- gate/dataset/celeba/celeba.py
+++ gate/dataset/celeba/celeba.py
@@ -9,9 +9,9 @@ from gate.data import data_loader
 class celeba(database.Database):
 
     def loads(self):
-        return data_loader.load_image_from_text(
-            self.data_path, self.shuffle, self.data_type, self.image,
-            self.min_queue_num, self.batch_size, self.reader_thread)
+        return data_loader.load_image_from_text_multi_label(
+            self.data_path, self.shuffle, self.data_type, self.num_classes,
+            self.image, self.min_queue_num, self.batch_size, self.reader_thread)
 
     def __init__(self, data_type, name, chkp_path=None):
         # init basic class
@@ -24,6 +24,8 @@ class celeba(database.Database):
         self.min_queue_num = 32
         self._set_phase(data_type)
 
+        self.one_hot = True
+
         # image
         self.image = data_param.image()
         self.image.set_format(channels=3)
@@ -33,10 +35,10 @@ class celeba(database.Database):
 
         # log
         self.log = data_param.log(data_type, name, chkp_path)
-        self.log.set_log(print_frequency=20,
-                         save_summaries_iter=20,
-                         save_model_iter=200,
-                         test_interval=200)
+        self.log.set_log(print_frequency=50,
+                         save_summaries_iter=50,
+                         save_model_iter=1000,
+                         test_interval=1000)
 
         # setting hps
         self.hps = data_param.hps('lenet')
@@ -51,7 +53,7 @@ class celeba(database.Database):
         # lr
         self.lr = data_param.learning_rate()
         # self.lr.set_fixed(learning_rate=0.01)
-        self.lr.set_fixed(0.0002)
+        self.lr.set_fixed(5e-5)
 
         # show
         self._print()
@@ -66,9 +68,9 @@ class celeba(database.Database):
 
     def _train(self):
         # basic param
-        self.batch_size = 8
+        self.batch_size = 64
         self.total_num = 202599
         self.name = self.name + '_train'
-        self.reader_thread = 1
+        self.reader_thread = 16
         self.shuffle = True
         self.data_path = '../_datasets/celeba/train.txt'
