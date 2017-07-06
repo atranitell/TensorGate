febf1b5565ad8c5c2ea9a2b8984843b0093e5a59
Kai JIN <atranitell@gmail.com>
2017-07-06 11:52:58 +0800

    remove

--- issue/cnn/regression_4view.py
+++ issue/cnn/regression_4view.py
@@ -430,27 +430,3 @@ def heatmap(name, chkp_path):
                 logger.test(f_str.get())
 
             return mean_mae, mean_rmse
-
-
-def pipline(name, chkp_path, gen_heatmap=False):
-    """ test all model in checkpoint file
-    """
-    import tools
-
-    chkp_file_path = os.path.join(chkp_path, 'checkpoint')
-    # make a backup
-    gate.utils.filesystem.copy_file(chkp_file_path, chkp_file_path+'.bk')
-    # acquire model list
-    chkp_model_list = tools.checkpoint.get_checkpoint_model_items(chkp_file_path)
-    # loop over the model list
-    for idx in range(1, len(chkp_model_list)):
-        # write list to new checkpoint file
-        new_model_list = chkp_model_list.copy()
-        new_model_list[0] = new_model_list[idx]
-        tools.checkpoint.write_checkpoint_model_items(chkp_file_path, new_model_list)
-        logger.info('Process model %s' % new_model_list[0])
-        # run test_all
-        if gen_heatmap:
-            heatmap(name, chkp_path)
-        else:
-            test(name, chkp_path)
