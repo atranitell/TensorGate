a572044dac41275a51d8626a4723ff2dfc435616
Kai JIN <atranitell@gmail.com>
2017-08-03 10:15:52 +0800

    add bn

--- gate/net/audionet.py
+++ gate/net/audionet.py
@@ -29,7 +29,7 @@ class AudioNet(net.Net):
     def model(self, inputs, num_classes, is_training):
         self.is_training = is_training
         self.activation_fn = tf.nn.relu
-        return self.audionet(inputs, num_classes, is_training, 'sen53_res')
+        return self.audionet(inputs, num_classes, is_training, 'sen17')
 
     def audionet(self, inputs, num_classes, is_training, kind):
         """ a factory corresponding to different experiments
@@ -72,8 +72,6 @@ class AudioNet(net.Net):
             scale=self.batch_norm_scale)
 
     def conv(self, x, num_filters, k_size, k_stride, name, act=True):
-        """ BN->RELU->Weight
-        """
         with tf.variable_scope(name):
             if act is True:
                 activation_fn = self.activation_fn
@@ -90,6 +88,23 @@ class AudioNet(net.Net):
                     stddev=0.01),
                 kernel_regularizer=layers.l2_regularizer(self.weight_decay),
                 bias_initializer=tf.constant_initializer(0.0))
+            return net
+
+    def conv_bn(self, x, num_filters, k_size, k_stride, name, act=True):
+        with tf.variable_scope(name):
+            net = tf.layers.conv1d(
+                inputs=x,
+                filters=num_filters,
+                kernel_size=k_size,
+                strides=k_stride,
+                padding='SAME',
+                activation=None,
+                kernel_initializer=tf.truncated_normal_initializer(
+                    stddev=0.01),
+                kernel_regularizer=layers.l2_regularizer(self.weight_decay),
+                bias_initializer=tf.constant_initializer(0.0))
+            if act is True:
+                net = self.bn(self.activation_fn(net))
             return net
 
     def activation_out(self, x, act_out):
@@ -123,6 +138,15 @@ class AudioNet(net.Net):
             x1 = self.conv(x, in_filters, k_size, 1, 'conv1_1')
             x2 = self.conv(x + x1, in_filters, k_size * 2, 1, 'conv1_2')
             x3 = self.conv(x + x2, in_filters, k_size, 1, 'conv1_3')
+            return self.activation_out(x + x3, act_out)
+
+    def ms_keep_bn(self, x, in_filters, k_size, name, act_out=True):
+        """ conv1 -> conv2 -> conv3
+        """
+        with tf.variable_scope(name):
+            x1 = self.conv_bn(x, in_filters, k_size, 1, 'conv1_1')
+            x2 = self.conv_bn(x + x1, in_filters, k_size * 2, 1, 'conv1_2')
+            x3 = self.conv_bn(x + x2, in_filters, k_size, 1, 'conv1_3')
             return self.activation_out(x + x3, act_out)
 
     def model_ms(self, inputs, num_classes, is_training, num_block=[3, 5, 5, 3]):
@@ -171,12 +195,60 @@ class AudioNet(net.Net):
 
             return logits, end_points
 
+
+    def model_ms_bn(self, inputs, num_classes, is_training, num_block=[3, 5, 5, 3]):
+        """ best model for paper
+        """
+        end_points = {}
+
+        with tf.variable_scope('ms_bn_' + itos(num_block)):
+            # root-3200
+            net = self.conv_bn(inputs, 64, 40, 2, name='root')
+            net = self.pool1d(net, 3, 2, name='max_pool0')
+
+            # block1-800
+            for i in range(num_block[0]):
+                net = self.ms_keep_bn(net, 64, 20, cat('k1', i))
+            net = self.conv_bn(net, 128, 20, 2, name='k1')
+
+            # block2-400
+            for i in range(num_block[1]):
+                net = self.ms_keep_bn(net, 128, 15, cat('k2', i))
+            net = self.conv_bn(net, 256, 15, 2, name='k2')
+
+            # block3-200
+            for i in range(num_block[2]):
+                net = self.ms_keep_bn(net, 256, 10, cat('k3', i))
+            net = self.conv_bn(net, 512, 10, 2, name='k3')
+
+            # block4-100
+            for i in range(num_block[3] - 1):
+                net = self.ms_keep_bn(net, 512, 5, cat('k4', i))
+            net = self.ms_keep_bn(net, 512, 5, cat('k4', num_block[3]), False)
+
+            # 100
+            end_points['gap_conv'] = net
+            net = tf.reduce_sum(net, axis=1)
+            net = layers.dropout(net, self.dropout, is_training=is_training)
+
+            logits = layers.fully_connected(
+                net, num_classes,
+                biases_initializer=None,
+                weights_initializer=tf.truncated_normal_initializer(
+                    stddev=0.01),
+                weights_regularizer=None,
+                activation_fn=None,
+                scope='logits')
+
+            return logits, end_points
+
+
     def model_ms_tiny(self, inputs, num_classes, is_training, num_block=[3, 5, 5, 3]):
         """
         """
         end_points = {}
 
-        with tf.variable_scope('model_ms4_' + itos(num_block)):
+        with tf.variable_scope('ms_tiny_' + itos(num_block)):
             # root-3200
             net = self.conv(inputs, 64, 40, 2, name='root')
             net = self.pool1d(net, 3, 2, name='max_pool0')
@@ -223,7 +295,7 @@ class AudioNet(net.Net):
         """
         end_points = {}
 
-        with tf.variable_scope('model_ms_res' + itos(num_block)):
+        with tf.variable_scope('ms_res_' + itos(num_block)):
             # root-3200
             net = self.conv(inputs, 64, 40, 2, name='root')
             net = self.pool1d(net, 3, 2, name='max_pool0')
@@ -270,7 +342,7 @@ class AudioNet(net.Net):
         """
         end_points = {}
 
-        with tf.variable_scope('model_ms_dense' + itos(num_block)):
+        with tf.variable_scope('ms_dense_' + itos(num_block)):
             # root-3200
             net = self.conv(inputs, 64, 40, 2, name='root')
             net = self.pool1d(net, 3, 2, name='max_pool0')
