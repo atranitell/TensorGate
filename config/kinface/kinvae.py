f1306e72f4adcb192da909b9f3199373ffed07ff
KaiJIN <atranitell@gmail.com>
2017-11-25 11:40:52 +0800

    corrected protocal

--- config/kinface/kinvae.py
+++ config/kinface/kinvae.py
@@ -14,14 +14,14 @@ class kinvae():
     self.target = 'vae.kinvae'
     self.data_dir = '_datasets/mnist'
     self.phase = 'train'
-    self.output_dir = None
+    self.output_dir = None # '_outputs/mnist.vae.kinvae.171125112525/'
     self.device = '0'
 
     self.log = params.Log(
         print_invl=20,
         save_summaries_invl=10,
         save_model_invl=500,
-        test_invl=500,
+        test_invl=2000,
         val_invl=500,
         max_iter=999999)
 
@@ -47,6 +47,8 @@ class kinvae():
       self._train()
     elif phase == 'test':
       self._test()
+    elif phase == 'val':
+      self._val()
     else:
       raise ValueError('Unknown phase [%s]' % phase)
 
@@ -58,7 +60,7 @@ class kinvae():
         batchsize=64,
         entry_path="_datasets/kinface2/train.txt",
         shuffle=True,
-        total_num=1000,
+        total_num=800,
         loader='load_pair_image_from_text')
     self.data.add_image(self.image)
     self.data.add_image(self.image)
@@ -80,8 +82,22 @@ class kinvae():
         batchsize=100,
         entry_path="_datasets/kinface2/test.txt",
         shuffle=False,
+        total_num=400,
+        loader='load_pair_image_from_text',
+        reader_thread=1)
+    self.data.add_image(self.image)
+    self.data.add_image(self.image)
+    self.data.label(num_classes=4)
+
+  def _val(self):
+    self.phase = 'val'
+    self.data = params.Data(
+        batchsize=100,
+        entry_path="_datasets/kinface2/test.txt",
+        shuffle=False,
         total_num=100,
-        loader='load_pair_image_from_text')
+        loader='load_pair_image_from_text',
+        reader_thread=1)
     self.data.add_image(self.image)
     self.data.add_image(self.image)
     self.data.label(num_classes=4)
