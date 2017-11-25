dccc42c06b1685ac5b3f42aac0141980f71ba8d2
KaiJIN <atranitell@gmail.com>
2017-11-25 11:27:53 +0800

    add image helper

--- core/utils/image.py
+++ core/utils/image.py
@@ -1,25 +1,41 @@
 # -*- coding: utf-8 -*-
 """ Save Image  - updated: 2017/05/09
 """
-
 import scipy.misc
 import numpy as np
+from PIL import Image
+from core import utils
 
 
-def save_images(images, size, image_path):
+def saveall(dst, imgs, paths):
+  """ save all imgs to dst dir with name(paths)
+  """
+  for idx, content in enumerate(imgs):
+    dst = utils.path.join_name(dst, paths[idx])
+    save(content, dst)
+
+
+def save(image, path):
+  """ Save a numpy array to path.
+    Need not scale to 255.
+  """
+  return scipy.misc.imsave(path, image)
+
+
+def save_images(images, size, path):
   """Save mutiple images into one single image.
   Parameters
   -----------
   images : numpy array [batch, w, h, c]
   size : list of two int, row and column number.
       number of images should be equal or less than size[0] * size[1]
-  image_path : string.
+  path : string.
   Examples
   ---------
   >>> images = np.random.rand(64, 100, 100, 3)
   >>> save_images(images, [8, 8], 'temp.png')
   """
-  def merge(images, size):
+  def merge_images(images, size):
     h, w = images.shape[1], images.shape[2]
     img = np.zeros((h * size[0], w * size[1], 3))
     for idx, image in enumerate(images):
@@ -27,9 +43,4 @@ def save_images(images, size, image_path):
       j = idx // size[1]
       img[j * h: j * h + h, i * w:i * w + w, :] = image
     return img
-
-  def imsave(images, size, path):
-    # images = (images + 1.) / 2.
-    return scipy.misc.imsave(path, merge(images, size))
-
-  return imsave(images, size, image_path)
+  return save(merge_images(images, size), path)
