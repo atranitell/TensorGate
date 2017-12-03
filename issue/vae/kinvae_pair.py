a24d76ea5c839d6adf62060fed97e54794c4bfd5
KaiJIN <atranitell@gmail.com>
2017-12-03 13:45:45 +0800

    tidy up variables names and func

--- issue/vae/kinvae_pair.py
+++ issue/vae/kinvae_pair.py
@@ -17,30 +17,23 @@ import numpy as np
 
 
 class KIN_VAE_PAIR(context.Context):
-  """
-  """
+  """ """
 
   def __init__(self, config):
     context.Context.__init__(self, config)
 
-  def _encoder(self, x, reuse=None):
-    return kin_vae.encoder(x, self.config.net.z_dim, self.is_train, reuse)
+  def _encoder(self, x, cond, reuse=None):
+    return kin_vae.encoder(x, cond, self.data.num_classes,
+                           self.config.net.z_dim, self.is_train, reuse)
 
-  def _generator(self, z, reuse=None):
-    return kin_vae.generator(z, self.is_train, reuse)
+  def _generator(self, z, cond, reuse=None):
+    return kin_vae.generator(z, cond, self.is_train, reuse)
 
-  def _discriminator(self, x, reuse=None):
-    return kin_vae.discriminator(x, self.is_train, reuse)
+  def _discriminator(self, x, cond, reuse=None):
+    return kin_vae.discriminator(x, cond, self.data.num_classes,
+                                 self.is_train, reuse)
 
-  def _recognitor(self, x1, x2, label):
-    _, _, feat_c1 = self._encoder(x1, reuse=True)
-    _, _, feat_p2 = self._encoder(x2, reuse=True)
-    losses, loss = cosine.get_loss(feat_c1, feat_p2, label,
-                                   self.data.batchsize,
-                                   is_training=self.is_train)
-    return losses, loss, feat_c1, feat_p2
-
-  def _loss(self, real, fake, mu, sigma):
+  def _loss_vae(self, real, fake, mu, sigma):
     """
     real: real images
     fake: generative images
@@ -51,10 +44,8 @@ class KIN_VAE_PAIR(context.Context):
         real * tf.log(fake) + (1 - real) * tf.log(1 - fake), [1, 2])
     KL_divergence = 0.5 * tf.reduce_sum(
         tf.square(mu) + tf.square(sigma) - tf.log(1e-8 + tf.square(sigma)) - 1, [1])
-
     neg_loglikelihood = -tf.reduce_mean(marginal_likelihood)
     KL_divergence = tf.reduce_mean(KL_divergence)
-
     ELBO = -neg_loglikelihood - KL_divergence
     return -ELBO
 
@@ -72,37 +63,48 @@ class KIN_VAE_PAIR(context.Context):
         labels=tf.ones_like(D_F), logits=D_F))
     return D_loss, G_loss
 
+  def _loss_metric(self, feat_x, feat_y, label):
+    return cosine.get_loss(feat_x, feat_y, label,
+                           self.data.batchsize,
+                           is_training=self.is_train)
+
+  def _write_feat_to_npy(self, idx, x, y, label):
+    """ fast to record x1, x2, label to npy array """
+    if self.phase == 'val':
+      self.val_x = x if idx == 0 else np.row_stack((self.val_x, x))
+      self.val_y = y if idx == 0 else np.row_stack((self.val_y, y))
+      self.val_l = label if idx == 0 else np.append(self.val_l, label)
+    elif self.phase == 'test':
+      self.test_x = x if idx == 0 else np.row_stack((self.test_x, x))
+      self.test_y = y if idx == 0 else np.row_stack((self.test_y, y))
+      self.test_l = label if idx == 0 else np.append(self.test_l, label)
+
   def train(self):
-    """
-    """
+    """ """
     # set phase
     self._enter_('train')
 
     # get data pipeline
-    data, label, path = loads(self.config)
-    real_c1, real_p1, real_p2 = tf.unstack(data, axis=1)
+    data, info, path = loads(self.config)
+    c1_real, p1_real, p2_real = tf.unstack(data, axis=1)
+    label, cond = tf.unstack(info, axis=1)
 
     # encode image to a vector
-    mu, sigma, feat_c1 = self._encoder(real_c1)
+    c1_mu, c1_sigma, feat_c1 = self._encoder(c1_real, cond)
+    p2_mu, p2_sigma, feat_p2 = self._encoder(p2_real, cond, True)
 
     # resample from the re-parameterzation
-    z = mu + sigma * tf.random_normal(tf.shape(mu))
-
-    # decoding
-    fake = self._generator(z)
-    fake = tf.clip_by_value(fake, 1e-8, 1 - 1e-8)
+    c1_z = c1_mu + c1_sigma * tf.random_normal(tf.shape(c1_mu))
+    c1_fake = tf.clip_by_value(self._generator(c1_z, cond), 1e-8, 1 - 1e-8)
 
     # discriminator
-    D_fake, D_net_fake = self._discriminator(fake, reuse=False)
-    D_real1, D_net_real1 = self._discriminator(real_p1, reuse=True)
-
-    # recognitor
-    R_loss, _, _, _ = self._recognitor(fake, real_p2, label)
+    D_c1_fake, D_net_c1 = self._discriminator(c1_fake, cond, reuse=False)
+    D_p1_real, D_net_p1 = self._discriminator(p1_real, cond, reuse=True)
 
     # loss
-    E_loss = self._loss(real_p1, fake, mu, sigma)
-    D_loss, G_loss = self._loss_gan(D_fake, D_real1)
-
+    R_loss, R_loss_batch = self._loss_metric(feat_c1, feat_p2, label)
+    E_loss = self._loss_vae(p1_real, c1_fake, c1_mu, c1_sigma)
+    D_loss, G_loss = self._loss_gan(D_c1_fake, D_p1_real)
     loss = E_loss + D_loss + G_loss + R_loss
 
     # # allocate two optimizer
@@ -145,30 +147,50 @@ class KIN_VAE_PAIR(context.Context):
         sess.run(train_op)
 
   def _val_or_test(self, dstdir):
+    """ COMMON FOR TRAIN AND VAL """
+
     # considering output train image
-    data, label, path = loads(self.config)
-    real1, real2 = tf.unstack(data, axis=1)
+    data, info, path = loads(self.config)
+    c1_real, p1_real, p2_real = tf.unstack(data, axis=1)
+    label, cond = tf.unstack(info, axis=1)
+
     # encode image to a vector
-    mu, sigma, feat_c1 = self._encoder(real1)
-    fake = tf.clip_by_value(self._generator(mu), 1e-8, 1 - 1e-8)
-    _, loss, feat_c1, feat_p2 = self._recognitor(fake, real2, label)
+    c1_mu, c1_sigma, feat_c1 = self._encoder(c1_real, cond)
+    p2_mu, p2_sigma, feat_p2 = self._encoder(p2_real, cond, True)
+
+    # resample from the re-parameterzation
+    # without std
+    c1_z = c1_mu
+    c1_fake = tf.clip_by_value(self._generator(c1_z, cond), 1e-8, 1 - 1e-8)
+
+    # loss
+    R_loss, loss = self._loss_metric(feat_c1, feat_p2, label)
+
     # saver
     saver = tf.train.Saver()
-    num_iter = int(self.data.total_num / self.data.batchsize)
+    batchsize = self.data.batchsize
+    num_iter = int(self.data.total_num / batchsize)
+
     with tf.Session() as sess:
+      # load snapshot from filepath
       step = self.snapshot.restore(sess, saver)
-      info = utils.string.concat(self.data.batchsize, [path, label, loss])
+      info = utils.string.concat(batchsize, [path, label, loss])
+      output = [c1_fake, p1_real, p2_real, path, info, feat_c1, feat_p2, label]
       # img_dir = utils.filesystem.mkdir(dstdir + step)
+
       with open(dstdir + '%s.txt' % step, 'wb') as fw:
         with context.QueueContext(sess):
           for i in range(num_iter):
-            imgs, paths, _info, _x, _y, _label = sess.run(
-                [fake, path, info, feat_c1, feat_p2, label])
-            self.write_feat_to_npy(i, _x, _y, _label)
-            # utils.image.saveall(img_dir, imgs, paths)
-            utils.image.save_images(
-                images=imgs, size=[10, 10],
-                path=utils.path.join_step(dstdir, step, 'png'))
+            # running session
+            _c1, _p1, _p2, _path, _info, _x, _y, _label = sess.run(output)
+            # to compute PCA
+            self._write_feat_to_npy(i, _x, _y, _label)
+            # save all iamges to folder
+            # utils.image.saveall(img_dir, _fake, _path)
+            utils.image.save_batch(_c1, batchsize, dstdir, step, '_c1')
+            utils.image.save_batch(_p1, batchsize, dstdir, step, '_p1')
+            utils.image.save_batch(_p2, batchsize, dstdir, step, '_p2')
+            # write to file
             [fw.write(_line + b'\r\n') for _line in _info]
 
   def test(self):
@@ -187,15 +209,3 @@ class KIN_VAE_PAIR(context.Context):
     val_dir = utils.filesystem.mkdir(self.config.output_dir + '/val/')
     self._val_or_test(val_dir)
     self._exit_()
-
-  def write_feat_to_npy(self, idx, x, y, label):
-    """ fast to record x1, x2, label to npy array
-    """
-    if self.phase == 'val':
-      self.val_x = x if idx == 0 else np.row_stack((self.val_x, x))
-      self.val_y = y if idx == 0 else np.row_stack((self.val_y, y))
-      self.val_l = label if idx == 0 else np.row_stack((self.val_l, label))
-    elif self.phase == 'test':
-      self.test_x = x if idx == 0 else np.row_stack((self.test_x, x))
-      self.test_y = y if idx == 0 else np.row_stack((self.test_y, y))
-      self.test_l = label if idx == 0 else np.row_stack((self.test_l, label))
