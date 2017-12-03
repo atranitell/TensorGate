1c8c673e3443b73bd55629d830df5d6677e26628
KaiJIN <atranitell@gmail.com>
2017-12-03 09:35:50 +0800

    without augmentation

--- core/database/preprocessing/vae/kinship_preprocessing.py
+++ core/database/preprocessing/vae/kinship_preprocessing.py
@@ -8,19 +8,18 @@ def preprocess_for_train(image, output_height, output_width):
   """ Preprocesses the given image for training."""
   # Transform the image to floats.
   image = tf.to_float(image) / 255.
-  resized_image = tf.image.resize_images(
-      image, (output_height, output_width))
-  resized_image = tf.image.random_flip_left_right(resized_image)
-  return resized_image
+  # image = tf.pad(image, [[4, 4], [4, 4], [0, 0]])
+  # image = tf.random_crop(image, [output_height, output_width, 3])
+  # image = tf.image.random_flip_left_right(image)
+  return image
 
 
 def preprocess_for_eval(image, output_height, output_width):
   """ Preprocesses the given image for evaluation. """
   # Transform the image to floats.
   image = tf.to_float(image) / 255.
-  resized_image = tf.image.resize_images(
-      image, (output_height, output_width))
-  return resized_image
+  # image = tf.image.resize_images(image, (output_height, output_width))
+  return image
 
 
 def preprocess_image(image, output_height, output_width, is_training=False):
