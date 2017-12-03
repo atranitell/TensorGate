b4417a6e3a58cf9bd10f33b132835f73aef11f78
KaiJIN <atranitell@gmail.com>
2017-12-03 09:47:16 +0800

    add extract feature

--- issue/vae/kinvae_pair.py
+++ issue/vae/kinvae_pair.py
@@ -9,7 +9,11 @@ from core.solver import updater
 from core.solver import variables
 from core.loss import cosine
 from core import utils
+from core.utils.logger import logger
 from issue import context
+
+from config.datasets.kinface.kinface_utils import Error
+import numpy as np
 
 
 class KIN_VAE_PAIR(context.Context):
@@ -20,31 +24,21 @@ class KIN_VAE_PAIR(context.Context):
     context.Context.__init__(self, config)
 
   def _encoder(self, x, reuse=None):
-    return kin_vae.encoder(
-        self.data.batchsize, x,
-        z_dim=self.config.net.z_dim,
-        is_training=self.is_train,
-        reuse=reuse)
+    return kin_vae.encoder(x, self.config.net.z_dim, self.is_train, reuse)
 
-  def _decoder(self, z, reuse=None):
-    return kin_vae.decoder(self.data.batchsize, z,
-                           self.is_train, reuse)
-
-  def _classifier(self, x, reuse=None):
-    return kin_vae.classifier(x, self.data.num_classes,
-                              self.is_train, reuse)
+  def _generator(self, z, reuse=None):
+    return kin_vae.generator(z, self.is_train, reuse)
 
   def _discriminator(self, x, reuse=None):
-    return kin_vae.discriminator(
-        self.data.batchsize, x,
-        self.is_train, reuse)
+    return kin_vae.discriminator(x, self.is_train, reuse)
 
   def _recognitor(self, x1, x2, label):
-    feat1 = kin_vae.recognitor(x1, self.is_train, 'net1')
-    feat2 = kin_vae.recognitor(x2, self.is_train, 'net2')
-    return cosine.get_loss(feat1, feat2, label,
-                           self.data.batchsize,
-                           is_training=self.is_train)
+    _, _, feat_c1 = self._encoder(x1, reuse=True)
+    _, _, feat_p2 = self._encoder(x2, reuse=True)
+    losses, loss = cosine.get_loss(feat_c1, feat_p2, label,
+                                   self.data.batchsize,
+                                   is_training=self.is_train)
+    return losses, loss, feat_c1, feat_p2
 
   def _loss(self, real, fake, mu, sigma):
     """
@@ -74,10 +68,8 @@ class KIN_VAE_PAIR(context.Context):
     D_loss_R = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
         labels=tf.ones_like(D_R), logits=D_R))
     D_loss = D_loss_F + D_loss_R
-
     G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
         labels=tf.ones_like(D_F), logits=D_F))
-
     return D_loss, G_loss
 
   def train(self):
@@ -91,13 +83,13 @@ class KIN_VAE_PAIR(context.Context):
     real_c1, real_p1, real_p2 = tf.unstack(data, axis=1)
 
     # encode image to a vector
-    mu, sigma = self._encoder(real_c1)
+    mu, sigma, feat_c1 = self._encoder(real_c1)
 
     # resample from the re-parameterzation
     z = mu + sigma * tf.random_normal(tf.shape(mu))
 
     # decoding
-    fake = self._decoder(z)
+    fake = self._generator(z)
     fake = tf.clip_by_value(fake, 1e-8, 1 - 1e-8)
 
     # discriminator
@@ -105,7 +97,7 @@ class KIN_VAE_PAIR(context.Context):
     D_real1, D_net_real1 = self._discriminator(real_p1, reuse=True)
 
     # recognitor
-    R_loss = self._recognitor(fake, real_p2, label)
+    R_loss, _, _, _ = self._recognitor(fake, real_p2, label)
 
     # loss
     E_loss = self._loss(real_p1, fake, mu, sigma)
@@ -117,18 +109,13 @@ class KIN_VAE_PAIR(context.Context):
     global_step = tf.train.create_global_step()
 
     var_e = variables.select_vars('encoder')
-    var_g = variables.select_vars('decoder')
+    var_g = variables.select_vars('generator')
     var_d = variables.select_vars('discriminator')
-    var_r = variables.select_vars('recognitor')
 
     op1 = updater.default(self.config, loss, global_step, var_e, 0)
     op2 = updater.default(self.config, loss, None, var_g, 1)
     op3 = updater.default(self.config, loss, None, var_d, 0)
-    op4 = updater.default(self.config, loss, None, var_r, 2)
-    train_op = tf.group(op1, op2, op3, op4)
-
-    # train_op = updater.default(
-    #     self.config, , global_step)
+    train_op = tf.group(op1, op2, op3)
 
     # update at the same time
     saver = tf.train.Saver(var_list=variables.all())
@@ -139,7 +126,7 @@ class KIN_VAE_PAIR(context.Context):
     running_hook = context.Running_Hook(
         config=self.config.log,
         step=global_step,
-        keys=['ELBO', 'D', 'G', 'R'],
+        keys=['E', 'D', 'G', 'R'],
         values=[E_loss, D_loss, G_loss, R_loss],
         func_test=self.test,
         func_val=self.val)
@@ -157,65 +144,58 @@ class KIN_VAE_PAIR(context.Context):
       while not sess.should_stop():
         sess.run(train_op)
 
-  def test(self):
-    """ random test a group of image
-    """
-    self._enter_('test')
-    test_dir = utils.filesystem.mkdir(self.config.output_dir + '/test/')
-
+  def _val_or_test(self, dstdir):
     # considering output train image
     data, label, path = loads(self.config)
     real1, real2 = tf.unstack(data, axis=1)
-
     # encode image to a vector
-    mu, sigma = self._encoder(real1)
-    z = mu + sigma * tf.random_normal(tf.shape(mu))
-    fake = self._decoder(z)
-    fake = tf.clip_by_value(fake, 1e-8, 1 - 1e-8)
-    loss = self._recognitor(fake, real2, label)
-
+    mu, sigma, feat_c1 = self._encoder(real1)
+    fake = tf.clip_by_value(self._generator(mu), 1e-8, 1 - 1e-8)
+    _, loss, feat_c1, feat_p2 = self._recognitor(fake, real2, label)
+    # saver
     saver = tf.train.Saver()
     num_iter = int(self.data.total_num / self.data.batchsize)
     with tf.Session() as sess:
       step = self.snapshot.restore(sess, saver)
-      print(loss)
       info = utils.string.concat(self.data.batchsize, [path, label, loss])
-      with open(test_dir + '%s.txt' % step, 'wb') as fw:
-        # start to test
-        img_dir = utils.filesystem.mkdir(test_dir + step)
+      # img_dir = utils.filesystem.mkdir(dstdir + step)
+      with open(dstdir + '%s.txt' % step, 'wb') as fw:
         with context.QueueContext(sess):
           for i in range(num_iter):
-            imgs, paths, _info = sess.run([fake, path, info])
+            imgs, paths, _info, _x, _y, _label = sess.run(
+                [fake, path, info, feat_c1, feat_p2, label])
+            self.write_feat_to_npy(i, _x, _y, _label)
             # utils.image.saveall(img_dir, imgs, paths)
+            utils.image.save_images(
+                images=imgs, size=[10, 10],
+                path=utils.path.join_step(dstdir, step, 'png'))
             [fw.write(_line + b'\r\n') for _line in _info]
 
+  def test(self):
+    self._enter_('test')
+    test_dir = utils.filesystem.mkdir(self.config.output_dir + '/test/')
+    self._val_or_test(test_dir)
+    val_err, val_thed, test_err = Error().get_all_result(
+        self.val_x, self.val_y, self.val_l,
+        self.test_x, self.test_y, self.test_l, True)
+    logger.test('val error:%f, thred:%f, test error:%f' %
+                (val_err, val_thed, test_err))
     self._exit_()
 
   def val(self):
-    """ random test a group of image
-    """
     self._enter_('val')
     val_dir = utils.filesystem.mkdir(self.config.output_dir + '/val/')
-
-    # considering output train image
-    data, label, path = loads(self.config)
-    real1, real2 = tf.unstack(data, axis=1)
-
-    mu, sigma = self._encoder(real1)
-    z = mu + sigma * tf.random_normal(tf.shape(mu))
-    fake = self._decoder(z)
-    fake = tf.clip_by_value(fake, 1e-8, 1 - 1e-8)
-
-    saver = tf.train.Saver(allow_empty=True)
-    num_iter = int(self.data.total_num / self.data.batchsize)
-
-    with tf.Session() as sess:
-      step = self.snapshot.restore(sess, saver)
-      with context.QueueContext(sess):
-        for _ in range(num_iter):
-          imgs = sess.run(fake)
-          utils.image.save_images(
-              images=imgs, size=[10, 10],
-              path=utils.path.join_step(val_dir, step, 'png'))
-
+    self._val_or_test(val_dir)
     self._exit_()
+
+  def write_feat_to_npy(self, idx, x, y, label):
+    """ fast to record x1, x2, label to npy array
+    """
+    if self.phase == 'val':
+      self.val_x = x if idx == 0 else np.row_stack((self.val_x, x))
+      self.val_y = y if idx == 0 else np.row_stack((self.val_y, y))
+      self.val_l = label if idx == 0 else np.row_stack((self.val_l, label))
+    elif self.phase == 'test':
+      self.test_x = x if idx == 0 else np.row_stack((self.test_x, x))
+      self.test_y = y if idx == 0 else np.row_stack((self.test_y, y))
+      self.test_l = label if idx == 0 else np.row_stack((self.test_l, label))
