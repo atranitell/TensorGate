a1c7ee755eaf64fd4ffb5562ebc00b6fc6e76a46
KaiJIN <atranitell@gmail.com>
2017-12-22 23:49:09 +0800

    remove

--- config/factory.py
+++ config/factory.py
@@ -6,7 +6,6 @@
 from config.datasets.mnist import mnist
 from config.datasets.mnist import mnist_gan
 from config.datasets.mnist import mnist_regression
-from config.datasets.kinface import kinface
 from config.datasets.kinface import kinvae
 from config.datasets.kinface import kinvae_pair
 from config.datasets.kinface import kinface_npy
@@ -17,8 +16,8 @@ config_map = {
     'mnist.gan': mnist_gan.mnist_gan,
     'mnist.regression': mnist_regression.mnist_regression,
     'kinvae': kinvae.kinvae,
-    'kinvae.pair': kinvae_pair.kinvae_pair,
-    'kinface': kinface.kinface,
+    'kinvae1.pair': kinvae_pair.kinvae1_pair,
+    'kinvae2.pair': kinvae_pair.kinvae2_pair,
     'kinface.npy': kinface_npy.kinface_npy,
     'trafficflow': trafficflow.trafficflow
 }
