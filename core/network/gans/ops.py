e1ea36088e685487d602d9fccd83e797cde32961
KaiJIN <atranitell@gmail.com>
2017-12-22 23:49:24 +0800

    compatible with gan

--- core/network/gans/ops.py
+++ core/network/gans/ops.py
@@ -22,32 +22,34 @@ def conv_cond_concat(x, y):
   """Concatenate conditioning vector on feature map axis."""
   x_shapes = x.get_shape()
   y_shapes = y.get_shape()
-  return tf.concat([x, y * tf.ones([x_shapes[0], x_shapes[1], x_shapes[2], y_shapes[3]])], 3)
+  cond = tf.ones([x_shapes[0], x_shapes[1], x_shapes[2], y_shapes[3]])
+  return tf.concat([x, y * cond], 3)
 
 
-def conv2d(x, filters, ksize, stride, name="conv2d"):
+def conv2d(x, filters, ksize, stride, name="conv2d", padding='SAME'):
   return tf.layers.conv2d(
       inputs=x,
       filters=filters,
       kernel_size=ksize,
       strides=stride,
-      padding='VALID',
+      padding=padding,
       kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
+      kernel_regularizer=layers.l2_regularizer(0.0005),
       name=name)
 
 
-def deconv2d(x, filters, ksize, stride, name="deconv2d"):
+def deconv2d(x, filters, ksize, stride, name="deconv2d", padding='SAME'):
   return tf.layers.conv2d_transpose(
       inputs=x,
       filters=filters,
       kernel_size=ksize,
       strides=stride,
-      padding='VALID',
+      padding=padding,
       kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
       name=name)
 
 
-def lrelu(x, leak=0.2, name='leak_relu'):
+def lrelu(x, leak=0.2, name=None):
   return tf.maximum(x, leak * x)
 
 
