293259af04ac0d73f589fbed07cf6ad91bf09aab
KaiJIN <atranitell@gmail.com>
2017-12-31 16:15:45 +0800

    a more concise interface to gate

--- gate.py
+++ gate.py
@@ -6,7 +6,6 @@
 import os
 import sys
 import argparse
-from datetime import datetime
 
 # hidden device output
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -16,88 +15,60 @@ os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = sys.argv[1]
 
 from config import factory
-from core.utils import filesystem
-from core.utils.logger import logger
-
-OUTPUTS = filesystem.mkdir('../_outputs/')
 
 
-class Gate():
+def run(name, extra):
+  config = factory.get(name, extra)
 
-  def __init__(self):
-    """
-    """
-    self.config = None
-    self.pid = datetime.strftime(datetime.now(), '%y%m%d%H%M%S')
+  """Target"""
+  # For CNN
+  if config.target == 'cnn.classification':
+    from issue.cnn.classification import classification as App
+  elif config.target == 'cnn.regression':
+    from issue.cnn.regression import regression as App
+  elif config.target == 'cnn.pairwise':
+    from issue.cnn.pairwise import pairwise as App
+  # For GAN
+  elif config.target == 'gan.dcgan':
+    from issue.gan.dcgan import DCGAN as App
+  elif config.target == 'gan.cgan':
+    from issue.gan.cgan import CGAN as App
+  elif config.target == 'gan.cwgan':
+    from issue.gan.cwgan import CWGAN as App
+  elif config.target == 'gan.acgan':
+    from issue.gan.acgan import ACGAN as App
+  # For VAE
+  elif config.target == 'vae.cvae':
+    from issue.vae.cvae import CVAE as App
+  elif config.target == 'vae.cvae.gan':
+    from issue.vae.cvae_gan import CVAE_GAN as App
+  elif config.target.find('kinvae') == 0:
+    from issue.kinface.kinface import select as App
+  # For ML
+  elif config.target == 'ml.active.sampler':
+    from issue.ml.active_sampler import active_sampler as App
+  elif config.target == 'ml.trafficflow':
+    from issue.ml.trafficflow import trafficflow as App
+  elif config.target == 'ml.extract_feature':
+    from issue.ml.extract_feature import EXTRACT_FEATURE as App
+  # Unkown
+  else:
+    raise ValueError('Unknown target [%s]' % config.target)
 
-  def initilize(self, name, extra=None):
-    # 0. load config params, default of 'train'
-    config = factory.get(name, extra)
+  # init
+  app = App(config)
 
-    # 1. setting output dir
-    if config.output_dir is None:
-      config.output_dir = filesystem.mkdir(
-          OUTPUTS + config.name + '.' + config.target + '.' + self.pid)
-
-    # 2. setting logger location
-    logger.init(config.name + '.' + self.pid, config.output_dir)
-    logger.info('Initilized logger successful.')
-    logger.info('Current model in %s' % config.output_dir)
-
-    # 3. save config as own
-    self.config = config
-
-  def run(self):
-    """Target"""
-    # For CNN
-    if self.config.target == 'cnn.classification':
-      from issue.cnn.classification import classification as App
-    elif self.config.target == 'cnn.regression':
-      from issue.cnn.regression import regression as App
-    elif self.config.target == 'cnn.pairwise':
-      from issue.cnn.pairwise import pairwise as App
-    # For GAN
-    elif self.config.target == 'gan.dcgan':
-      from issue.gan.dcgan import DCGAN as App
-    elif self.config.target == 'gan.cgan':
-      from issue.gan.cgan import CGAN as App
-    elif self.config.target == 'gan.cwgan':
-      from issue.gan.cwgan import CWGAN as App
-    elif self.config.target == 'gan.acgan':
-      from issue.gan.acgan import ACGAN as App
-    # For VAE
-    elif self.config.target == 'vae.cvae':
-      from issue.vae.cvae import CVAE as App
-    elif self.config.target == 'vae.cvae.gan':
-      from issue.vae.cvae_gan import CVAE_GAN as App
-    elif self.config.target.find('kinvae') == 0:
-      from issue.kinface.kinface import select as App
-    # For ML
-    elif self.config.target == 'ml.active.sampler':
-      from issue.ml.active_sampler import active_sampler as App
-    elif self.config.target == 'ml.trafficflow':
-      from issue.ml.trafficflow import trafficflow as App
-    elif self.config.target == 'ml.extract_feature':
-      from issue.ml.extract_feature import EXTRACT_FEATURE as App
-    # Unkown
-    else:
-      raise ValueError('Unknown target [%s]' % self.config.target)
-
-    # init
-    app = App(self.config)
-
-    # for task
-    if self.config.phase == 'train':
-      app.train()
-    elif self.config.phase == 'test':
-      app.test()
-    elif self.config.phase == 'val':
-      app.val()
-    else:
-      raise ValueError('Unknown task [%s]' % self.config.phase)
-
-
-task = Gate()
+  # for task
+  if config.task == 'train':
+    app.train()
+  elif config.task == 'test':
+    app.test()
+  elif config.task == 'val':
+    app.val()
+  elif config.task == 'pipline':
+    app.pipline()
+  else:
+    raise ValueError('Unknown task [%s]' % config.task)
 
 
 if __name__ == "__main__":
@@ -105,6 +76,4 @@ if __name__ == "__main__":
   parser.add_argument('-name', type=str, dest='name')
   parser.add_argument('-extra', type=str, dest='extra', default=None)
   args, _ = parser.parse_known_args()
-
-  task.initilize(args.name, args.extra)
-  task.run()
+  run(args.name, args.extra)
--- issue/context.py
+++ issue/context.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
-""" Public base for CNN task
+""" Public base for All task
     updated: 2017/11/24
 """
 import time
-import tensorflow as tf
+import shutil
 import numpy as np
-
+import tensorflow as tf
+from datetime import datetime
 from core.utils.logger import logger
 from core.solver.snapshot import Snapshot
 from core.solver.summary import Summary
 from core.solver import variables
 from core.utils.string import print_members
+from core.utils import filesystem
+from tools import checkpoint
+
+OUTPUTS = filesystem.mkdir('../_outputs/')
 
 
 class Context():
@@ -19,11 +24,25 @@ class Context():
 
   def __init__(self, config):
     self.config = config
-    self.phase = config.phase
-    self.data = config.data
-    self.summary = Summary(config)
-    self.snapshot = Snapshot(config)
+    self._initialize()
+    #-- inner variables --#
+    self.phase = None
+    self.data = None
+    self.summary = Summary(self.config)
+    self.snapshot = Snapshot(self.config)
     print_members(self.config)
+
+  def _initialize(self):
+    pid = datetime.strftime(datetime.now(), '%y%m%d%H%M%S')
+    # 1. setting output dir
+    if self.config.output_dir is None:
+      self.config.output_dir = filesystem.mkdir(
+          OUTPUTS + self.config.name + '.' + self.config.target + '.' + pid)
+
+    # 2. setting logger location
+    logger.init(self.config.name + '.' + pid, self.config.output_dir)
+    logger.info('Initilized logger successful.')
+    logger.info('Current model in %s' % self.config.output_dir)
 
   def _enter_(self, phase):
     self.prephase = self.phase
@@ -32,6 +51,8 @@ class Context():
     self.data = self.config.data
 
   def _exit_(self):
+    if self.prephase is None:
+      return
     self.phase = self.prephase
     self.config.set_phase(self.phase)
     self.data = self.config.data
@@ -39,6 +60,26 @@ class Context():
   @property
   def is_train(self):
     return True if self.phase == 'train' else False
+
+  @property
+  def batchsize(self):
+    return self.config.data.batchsize
+
+  @property
+  def epoch_iter(self):
+    return int(self.config.data.total_num / self.config.data.batchsize)
+
+  def pipline(self):
+    """ traverse all checkpoints """
+    ckpt_path = self.config.output_dir + '/checkpoint'
+    ckpt_path_bk = ckpt_path + '.bk'
+    shutil.copy(ckpt_path, ckpt_path_bk)
+    model_list = checkpoint.get_checkpoint_model_items(ckpt_path)
+    for item in model_list[1:]:
+      model_list[0] = item
+      checkpoint.write_checkpoint_model_items(ckpt_path, model_list)
+      self.test()
+    shutil.copy(ckpt_path_bk, ckpt_path)
 
 
 class Running_Hook(tf.train.SessionRunHook):
