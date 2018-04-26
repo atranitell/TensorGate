4a64119c4ecdf7e8c9458b98feae6c3526e5157b
atranitell <atranitell@gmail.com>
2018-04-26 13:47:54 +0800

    fixed l2 reading method

--- gate/layer/l2.py
+++ gate/layer/l2.py
@@ -18,10 +18,10 @@ from gate.env import env
 
 
 def loss(logits, labels, config):
-  # range, batchsize, scale=True
+  # span, batchsize, scale=True
   """ l2 loss: |labels - logits|^2
   Args:
-      scale: true for scale labels from [0, range] to [0, 1]
+      scale: true for scale labels from [0, span] to [0, 1]
   Return:
       _losses: a scalr with float32
       _logits: a copy of logits with float32
@@ -31,7 +31,7 @@ def loss(logits, labels, config):
     _logits = tf.to_float(tf.reshape(logits, [config.data.batchsize, 1]))
     _labels = tf.to_float(tf.reshape(labels, [config.data.batchsize, 1]))
     if config.data.scale:
-      _labels = tf.divide(_labels, config.data.range)
+      _labels = tf.divide(_labels, config.data.span)
     _losses = tf.nn.l2_loss([_labels - _logits], name='loss')
     if env._SUMMARY_SCALAR:
       tf.summary.scalar('losses', _losses)
@@ -50,11 +50,10 @@ def error(logits, labels, config):
   with tf.name_scope('error'):
     _logits = tf.to_float(tf.reshape(logits, [config.data.batchsize, 1]))
     _labels = tf.to_float(tf.reshape(labels, [config.data.batchsize, 1]))
-    ranges = 1 if not config.data.scale else config.data.range
-    err_mae = tf.reduce_mean(input_tensor=tf.abs(
-        (_logits - _labels) * ranges), name='error_mae')
-    err_rmse = tf.sqrt(tf.reduce_mean(input_tensor=tf.square(
-        (_logits - _labels) * ranges), name='error_mse'))
+    _span = 1 if not config.data.scale else config.data.span
+    _error = _logits*_span - _labels
+    err_mae = tf.reduce_mean(tf.abs(_error, name='error_mae'))
+    err_rmse = tf.sqrt(tf.reduce_mean(tf.square(_error, name='error_mse')))
     if env._SUMMARY_SCALAR:
       tf.summary.scalar('mae', err_mae)
       tf.summary.scalar('rmse', err_rmse)
