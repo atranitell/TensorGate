99cd2f6b63ec8a080b6cee48cadcc4961dc0b6c3
atranitell <atranitell@gmail.com>
2018-04-27 12:21:08 +0800

    remove PCA module: deep learning is unnecessary

--- gate/layer/similarity.py
+++ gate/layer/similarity.py
@@ -14,7 +14,6 @@ Using for computing cosine similarity
 """
 
 import numpy as np
-import sklearn.decomposition.pca as pca
 
 
 def compute_cosine_similarity(feat1, feat2):
@@ -71,42 +70,15 @@ def get_result(feat_x, feat_y, labels, thred=None):
   return err, thed
 
 
-def get_all_result(v_x, v_y, v_l, t_x, t_y, t_l,
-                   use_PCA=False, use_val_thred=False):
-  # using PCA to reduce dim
-  if use_PCA:
-    v_x, v_y, t_x, t_y = transform_PCA(v_x, v_y, t_x, t_y)
-
+def get_all_result(v_x, v_y, v_l, t_x, t_y, t_l, use_val_thred=False):
   # compute thred in validation set
   val_err, val_thed = get_result(v_x, v_y, v_l)
-
   # compute error in test set
   if use_val_thred:
     test_err, test_thed = get_result(t_x, t_y, t_l, val_thed)
   else:
     test_err, test_thed = get_result(t_x, t_y, t_l)
-
   return val_err, test_thed, test_err
-
-
-def transform_PCA(val_x, val_y, test_x, test_y):
-  """ get new data via pca processing.
-  """
-  val_feats = np.row_stack((val_x, val_y))
-  test_feats = np.row_stack((test_x, test_y))
-
-  _pca = pca.PCA(n_components=200, svd_solver='full')
-  _pca.fit(val_feats)
-
-  val_feats = _pca.transform(val_feats)
-  test_feats = _pca.transform(test_feats)
-
-  _val_x = val_feats[:val_x.shape[0]]
-  _val_y = val_feats[val_x.shape[0]:]
-  _test_x = test_feats[:test_x.shape[0]]
-  _test_y = test_feats[test_x.shape[0]:]
-
-  return _val_x, _val_y, _test_x, _test_y
 
 
 def get_result_from_file(filepath, threshold=None):
@@ -119,7 +91,6 @@ def get_result_from_file(filepath, threshold=None):
       labels.append(int(r[4]))
       dists.append(float(r[5]))
   dists, labels = np.array(dists), np.array(labels)
-
   if threshold is None:
     return compute_best_error(dists, labels)
   else:
